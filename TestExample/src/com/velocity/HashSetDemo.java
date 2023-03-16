0d2b0dbadf966de7052b25eb10a99654403fb47d
Jeevan <jeevankulkarni4@gmail.com>
2023-03-16 16:46:50 +0530

    hashset code added

---
+++ TestExample/src/com/velocity/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.velocity;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> hs=new HashSet<String>();
+		hs.add("ajay");
+		hs.add("vikas");
+		hs.add("sohan");
+		System.out.println(hs);
+	}
+}

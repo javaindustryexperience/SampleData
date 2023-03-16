34c165048d6d416f94513b088ccf16f6882f8274
Jeevan <jeevankulkarni4@gmail.com>
2023-03-16 16:36:12 +0530

    arraylist code added

---
+++ TestExample/src/com/velocity/Test.java
@@ -0,0 +1,16 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("ramesh");
+		al.add("sandip");
+		al.add("amit");
+		al.add("ajay");
+		System.out.println(al);
+	}
+}
